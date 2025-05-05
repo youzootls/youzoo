7305ef86c00691f13d1c80e1b21818fc9d6bafa0
calabo-Act21 <camille.laborde@act21.fr>
2025-05-05 14:43:32 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <AppFooter></AppFooter>
           <Toaster />
         </main>
+        <Analytics />
       </body>
     </html>
   );

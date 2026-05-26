d3c87d0337f815e8ab3d39abdb5b4fcc5d38bca8
youzootls <160787221+youzootls@users.noreply.github.com>
2026-05-26 12:24:08 +0200

    Ajout de ratios de format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,13 @@ const config = {
 	],
 	prefix: "",
 	theme: {
+		aspectRatio: { // defaults to {}
+			'none': 0,
+			'square': [1, 1], // or 1 / 1, or simply 1
+			'16/9': [16, 9],  // or 16 / 9
+			'4/3': [4, 3],    // or 4 / 3
+			'21/9': [21, 9],  // or 21 / 9
+    	},
 		container: {
 			center: true,
 			padding: '2rem',
